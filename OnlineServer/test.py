b04cfa756186472096d4eee1341d68349eba557a
Zapetalk <105220195+Zapetalk@users.noreply.github.com>
2024-05-20 23:31:58 +0200

    Delete OnlineServer directory

--- OnlineServer/test.py
+++
@@ -1,8 +0,0 @@
-# coding=UTF-8
-QUESTION = "adsfasf"
-text_content = "fdsfsd"
-prompt = "Beantworte die folgende Frage, sollte sie zu den Themenbereich einer Schule passen:" + QUESTION \
-         + "Falls die Frage nicht passt antworte mit: \"Diese Frage befindet sich außerhalb meines Themenbereiches\"" \
-         + "Falls es Hilft benutze diese Information zum beantworten der Frage:" + text_content
-
-print(prompt)
