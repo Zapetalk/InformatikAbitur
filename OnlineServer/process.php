8f4ef1fb5b0509390c9846de15de467c970598bc
Zapetalk <105220195+Zapetalk@users.noreply.github.com>
2024-05-22 14:14:38 +0200

    Update process.php

--- OnlineServer/process.php
+++ OnlineServer/process.php
@@ -2,7 +2,8 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") { //Abfrage ob der Server eine Request Method POST bekommen hat 
     $prompt = $_POST["prompt"]; // Speichern von der Frage
 
-    $command = "python3 ChatBot.py " . escapeshellarg($prompt); // Formulierung eines Befehls der das Python Skript mit der Frage als Argument ausführt
+    // Formulierung eines Befehls der das Python Skript mit der Frage als Argument ausführt:                                       
+    $command = "python3 ChatBot.py " . escapeshellarg($prompt); 
     $output = shell_exec($command); // Führt den Befehl aus und Speichert den Output
 
     echo $output; // Gibt den Output zurück zur HTML-Seite
